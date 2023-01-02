6ebf0dcf6138e3b6b99ab0884e4a61703f8ab9ea
vrujeshmodi <vrujeshmodi@gmail.com>
2023-01-02 15:40:16 +0530

    Adding git ignore file

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Vrujesh Modi on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
